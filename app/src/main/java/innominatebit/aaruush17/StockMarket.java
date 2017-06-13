5ba60c3e3778c5cda84024e1fe0915cb32ed2cae
Abhinav Upadhyay <abhinavu1201@gmail.com>
2017-06-13 23:57:00 +0530

    Add code to fetch stock data

--- app/src/main/java/innominatebit/aaruush17/StockMarket.java
+++ app/src/main/java/innominatebit/aaruush17/StockMarket.java
@@ -3,13 +3,14 @@ package innominatebit.aaruush17;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class StockMarket extends AppCompatActivity
 {
@@ -23,11 +24,12 @@ public class StockMarket extends AppCompatActivity
     @Override
     protected void onStart() {
         super.onStart();
-
+        new FetchStockData().execute();
     }
-    class FetchData extends AsyncTask<Void,Void,Void>
+    class FetchStockData extends AsyncTask<Void,Void,Void>
     {
-        String murl="http://srmvdpauditorium.in/aaaTestApp.php?paramUsername=aaa&paramPassword=123",webPage="";
+        ArrayList<String> values;
+        String murl="http://srmvdpauditorium.in/SRMStockMarket/getStockData.php";
         @Override
         protected Void doInBackground(Void... voids)
         {
@@ -39,9 +41,15 @@ public class StockMarket extends AppCompatActivity
                 urlConnection = (HttpURLConnection) url.openConnection();
                 InputStreamReader isw = new InputStreamReader(urlConnection.getInputStream());
                 BufferedReader br=new BufferedReader(isw);
-                String data="";
+                String data="",webPage="";
                 while ((data=br.readLine()) != null)
-                    webPage=webPage+data+"\n";
+                    webPage=webPage+data;
+                values=new ArrayList<>();
+                while(webPage.indexOf('<')!=-1)
+                {
+                    webPage=webPage.substring(webPage.indexOf('<')+1);
+                    values.add(webPage.substring(0,webPage.indexOf('>')));
+                }
             }
             catch (IOException e)
             {
@@ -57,7 +65,20 @@ public class StockMarket extends AppCompatActivity
         @Override
         protected void onPostExecute(Void aVoid)
         {
-            //Toast.makeText(SomeActivity.this, webPage, Toast.LENGTH_SHORT).show();
+            int c=0;
+            ArrayList<Integer> ids=new ArrayList<>();
+            ids.add(R.id.hightech);             ids.add(R.id.eee);              ids.add(R.id.civil);
+            ids.add(R.id.placement);            ids.add(R.id.aaruush);          ids.add(R.id.aero);
+            ids.add(R.id.backgate);             ids.add(R.id.ub);               ids.add(R.id.techpark);
+            ids.add(R.id.archblock);            ids.add(R.id.bioblock);         ids.add(R.id.mba);
+            ids.add(R.id.ghostel);              ids.add(R.id.bhostel);          ids.add(R.id.audi);
+            ids.add(R.id.java);                 ids.add(R.id.hospital);         ids.add(R.id.vpt);
+            ids.add(R.id.dental);               ids.add(R.id.pfhostel);
+            while(c<20)
+            {
+                TextView t=(TextView)findViewById(ids.get(c));
+                t.setText(values.get(c++));
+            }
             super.onPostExecute(aVoid);
         }
     }
