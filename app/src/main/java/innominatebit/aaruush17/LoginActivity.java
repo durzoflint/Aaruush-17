f1213cd1012b10b45ed60c275a3496267633efe6
Abhinav Upadhyay <abhinavu1201@gmail.com>
2017-06-13 14:15:12 +0530

    Add AsyncTask to Check Connection

--- app/src/main/java/innominatebit/aaruush17/LoginActivity.java
+++ app/src/main/java/innominatebit/aaruush17/LoginActivity.java
@@ -1,7 +1,17 @@
 package innominatebit.aaruush17;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +20,40 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-
+    class FetchData extends AsyncTask<Void,Void,Void>
+    {
+        String murl="http://srmvdpauditorium.in/aaaTestApp.php?paramUsername=aaa&paramPassword=123",webPage="";
+        @Override
+        protected Void doInBackground(Void... voids)
+        {
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try
+            {
+                url = new URL(murl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStreamReader isw = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader br=new BufferedReader(isw);
+                String data="";
+                while ((data=br.readLine()) != null)
+                    webPage=webPage+data+"\n";
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            Toast.makeText(LoginActivity.this, webPage, Toast.LENGTH_SHORT).show();
+            super.onPostExecute(aVoid);
+        }
+    }
 }
